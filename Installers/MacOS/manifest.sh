93480c151a4d91840ae5927118d99d5e7f80f995
J. Gray <78924496+jgray1996@users.noreply.github.com>
2026-01-13 15:47:08 +0100

    Refactor manifest.sh for package installation paths
    Updated paths for installing BioConductor and GitHub packages. Removed unnecessary comments and commands.

--- Installers/MacOS/manifest.sh
+++ Installers/MacOS/manifest.sh
@@ -1,18 +1,12 @@
 #!/bin/zsh
 # Author: J.Gray
-# Email
 set -e
-
 source ~/.zshrc
-
 ~/.pixi/bin/pixi config set default-channels '["conda-forge", "bioconda"]' --global
-
 ~/.pixi/bin/pixi init dashboard
-
 cd dashboard
 
 ~/.pixi/bin/pixi add "r-base=4.4.3" "r-seurat" "r-devtools" "r-shiny" "r-tidyverse" "r-shinydashboard" "r-shinyjs" "r-shinyjqui" "r-shinyTree" "r-dt" "r-writexl" "r-data.table" "r-nnls" "r-mcmcpack" "r-e1071" "r-locfdr" "r-quadprog" "r-statmod" "r-corpcor" "r-doparallel" "r-ggally" "r-markdown" "r-xfun" "r-promises"
-# ~/.pixi/bin/pixi add "bioconductor-singlecellexperiment" "bioconductor-toast" "bioconductor-genomeinfodb"
 
 echo "✅ R version 4.4.3 installed succesfully!"
 
@@ -27,23 +21,17 @@ curl -L "https://github.com/jrdalenberg/UnBlender/archive/refs/heads/main.zip" -
 ## Extracting files
 echo "3️⃣\tExtracting dashboard!"
 unzip -X main.zip
-rm main.zip
 
 ## Removing reminants
-mv UnBlender-remove_unused_packages/Unblender-app .
-yes | rm -rf UnBlender-main
-
-# ======= Installing CRAN packages =======
-echo "4️⃣\tInstalling required CRAN packages"
-# ~/.pixi/bin/pixi run Rscript ../installR_scripts/install_CRAN_packages.R
+mv UnBlender-main/Unblender-app .
 
 # ======= Installing BioConductor packages =======
 echo "5️⃣\tInstalling required BioConductor packages"
-~/.pixi/bin/pixi run Rscript ../installR_scripts/install_BIOC_packages.R
+~/.pixi/bin/pixi run Rscript UnBlender-main/Installers/installR_scripts/install_BIOC_packages.R
 
 # ======= Installing Github packages =======
 echo "5️⃣\tInstalling required Github packages"
-~/.pixi/bin/pixi run Rscript ../installR_scripts/install_REPO_packages.R
+~/.pixi/bin/pixi run Rscript UnBlender-main/Installers/installR_scripts/install_REPO_packages.R
 
 # ======= Installing runtime documents =====
 curl -L "https://ironman.tenwisedev.nl/public/so_downs_5000.rds" -o Unblender-app/app/datafiles/so_downs_5000.rds
